7b715b65a32f6412ba67d56f50d1106f724d0361
irmabai <baixuejunirma@hotmail.com>
2017-04-24 18:56:48 +0800

    cachematrix.R Submission File

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The two functions below are a group used for caching and solving inverse matrix of a given
+## matrix, matrix could be input using the command like this: >myMatrix$set(matrix(data=c(0,1,1,0),nrow=2,ncol=2))
 
-## Write a short comment describing this function
+
+## makeCacheMatrix is used for creating matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m<-NULL
+        set<-function(y){
+                x<<-y
+                m<<-NULL
+        }
+        get<-function() x
+        setsolve<-function(solve) m<<-solve
+        getsolve<-function() m
+        list(set=set,get=get,setsolve=setsolve,getsolve=getsolve)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve is used for calculating inverse matrix using the solve() function built in R
 
 cacheSolve <- function(x, ...) {
+        m<-x$getsolve()
+        if(!is.null(m)){
+                message("getting cached data")
+                return(m)
+        }
+        data<-x$get()
+        m<-solve(data,...)
+        x$setsolve(m)
+        m
         ## Return a matrix that is the inverse of 'x'
 }
